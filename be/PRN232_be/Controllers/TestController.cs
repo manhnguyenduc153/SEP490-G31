154197ad4af548b2d7ac77d9b472d8e0df5471b8
Manh Nguyen <manhnguyenduc153@gmail.com>
2026-07-06 00:35:12 +0700

    test cicd

---
+++ be/PRN232_be/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRN232_be.DTO.Class;
+
+namespace PRN232_be.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("Heheh");
+        }
+    }
+}
