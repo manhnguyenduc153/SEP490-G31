696f0591b794d5322edaba597b1345ea8f1dcf70
linhmaideptrai <duylinh2992004@gmail.com>
2026-07-17 11:29:19 +0700

    report service

--- be/sep490_be/Models/ApplicationDbContext.cs
+++ be/sep490_be/Models/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System.Reflection;
