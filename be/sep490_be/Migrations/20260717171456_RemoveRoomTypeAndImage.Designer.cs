ccdd076dfb2f48162055ad6495cc89277fa51741
Manh Nguyen <manhnguyenduc153@gmail.com>
2026-07-18 00:48:00 +0700

    edit room

--- be/sep490_be/Migrations/20260717171456_RemoveRoomTypeAndImage.Designer.cs
+++ be/sep490_be/Migrations/20260717171456_RemoveRoomTypeAndImage.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sep490_be.Models;
 
@@ -11,9 +12,11 @@ using sep490_be.Models;
 namespace sep490_be.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260717171456_RemoveRoomTypeAndImage")]
+    partial class RemoveRoomTypeAndImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
